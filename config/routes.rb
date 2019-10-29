6193aaea7c7003c7b19341834b272afa743356ac
Hammy Wallace <Hammy.h.wallace@gmail.com>
2019-10-29 19:38:15 +0000

    9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "hams#index"
   resources :hams, only: [:new, :create]
 end
--- spec/controllers/hams_controller_spec.rb
+++ spec/controllers/hams_controller_spec.rb
@@ -25,4 +25,12 @@ RSpec.describe HamsController, type: :controller do
       expect(ham.message).to eq("Hello!")
     end
   end
+
+  it "should properly deal with validation errors" do
+    post :create, params: { ham: { message: '' } }
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(Gram.count).to eq 0
+  end
+
+
 end
